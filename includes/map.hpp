b3cfdca8cfdd6d9175f69e2453ab6676a99b999c
CVanderbilt <eduhgb5198@gmail.com>
2022-09-15 15:08:09 +0200

    added map

---
+++ includes/map.hpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include "avl_tree.hpp"
+
+namespace ft{
+
+
+template<
+    class Key,
+    class T,
+    class Compare = std::less<Key>,
+    class Allocator = std::allocator<std::pair<const Key, T> >
+> class map {
+	public:
+	/*
+	*	Member types
+	*/
+	typedef Key 						key_type;
+	typedef T 							mapped_type;
+	typedef std::pair<const Key, T>		value_type;
+	typedef std::size_t					size_type;
+	typedef std::ptrdiff_t				difference_type;
+	typedef Compare						key_compare;
+	typedef Allocator					allocator_type;
+	typedef value_type&					reference;
+	typedef const value_type&			const_reference;
+	typedef typename Allocator::pointer			pointer;
+	typedef typename Allocator::const_pointer	const_pointer;
+	/*iterator 	LegacyBidirectionalIterator to value_type
+	const_iterator 	LegacyBidirectionalIterator to const value_type
+	reverse_iterator 	std::reverse_iterator<iterator>
+	const_reverse_iterator 	std::reverse_iterator<const_iterator>*/
+
+	protected:
+	allocator_type			_alloc;
+	key_compare				_comp;
+	avl_tree <value_type>	_bst;
+
+	/*
+	*	Member class
+	*/
+	class value_compare : std::binary_function<value_type, value_type, bool>
+	{
+		friend class map<key_type, mapped_type, key_compare, allocator_type>;
+
+		protected:
+		key_compare comp;
+		value_compare (Compare c) : comp(c) {}
+
+		public:
+
+		bool operator() (const value_type& lhs, const value_type& rhs) const
+		{
+			return (comp(lhs.first, rhs.first));
+		}
+	};
+	/*
+	* Map code here
+	*/
+};
+}
